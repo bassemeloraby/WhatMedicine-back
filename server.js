4892961edb33408fa6fa1b84811a17f21e41a082
bassem eloraby <BASEM12683@GMAIL.COM>
2023-01-27 10:20:16 +0300

    express color

--- server.js
+++ server.js
@@ -1,1 +1,17 @@
-console.log('hello world')
+const express = require('express');
+const colors = require('colors');
+
+const app = express();
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+const port = process.env.PORT || '5000';
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+  })
+
+app.listen(port, (err) => {
+  if (err) throw err;
+  console.log('Server listening on port'.red, port.yellow);
+});
