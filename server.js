431d2f8e8c1491042a75bafc7741b1608606d8bc
bassem eloraby <BASEM12683@GMAIL.COM>
2023-01-27 10:35:42 +0300

    mongodb

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const colors = require('colors');
 const dotenv = require('dotenv').config()
-
+const connectDB = require('./config/db');
 
 const app = express();
 app.use(express.urlencoded({ extended: false }));
@@ -13,7 +13,15 @@ app.get('/', (req, res) => {
     res.send('Hello World!')
   })
 
-app.listen(port, (err) => {
-  if (err) throw err;
-  console.log('Server listening on port'.red, port.yellow);
-});
+//Connect to the database before listening
+connectDB().then(() => {
+    app.listen(port, (err) => {
+        if (err) throw err;
+        console.log('Server listening on port'.red, port.yellow);
+      });      
+})
+
+// app.listen(port, (err) => {
+//   if (err) throw err;
+//   console.log('Server listening on port'.red, port.yellow);
+// });
